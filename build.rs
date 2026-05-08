5eae51ddfe42e194fdaf4c6cb50ea1830b803e92
SED4906 <sed4906birdie@gmail.com>
2026-05-08 11:54:27 -0700

    Begin switching to wasmi

--- build.rs
+++ build.rs
@@ -1,9 +1,4 @@
 fn main() {
-    cc::Build::new()
-        .file("src/vsnprintf.c")
-        .flag("-Wno-builtin-declaration-mismatch")
-        .flag("-Wno-unused-parameter")
-        .compile("vsnprintf");
     // Tell cargo to pass the linker script to the linker..
     println!("cargo:rustc-link-arg=-Tlinker.ld");
     // ..and to re-run if it changes.
